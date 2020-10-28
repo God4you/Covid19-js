92960351a1708fbca35c755a316e5c7001b5d6c2
Benjamin Kakar <j0h@example.com>
2020-10-28 15:07:18 +0100

    Bg change and language translation and data change.

--- main.js
+++ main.js
@@ -6,21 +6,22 @@ let $myNewCases = $('#myLiveData');
 async function getCovidApi(){
     const jsondata = await fetch(url);
     const jsdata = await jsondata.json();
-    //console.log(jsdata)
+    console.log(jsdata)
     const item=jsdata.Countries[46];//Denmark
-    //console.log(item)
+    console.log(item)
     //Total Cases
-    $myTotalCases.append(                   
-    `<td>${item.Date}</td>
+    $myTotalCases.append(     //<td>${item.Date}</td>              
+    `       
+    
     <td>${item.TotalConfirmed}</td>
     <td style="color: red;">${item.TotalDeaths}</td>
     <td>${item.TotalRecovered}</td>
     `)
     //New Cases
-    $myNewCases.append(                   
-    `<td>${item.Date}</td>
+    $myNewCases.append(                   //<td>${item.Date}</td>
+    `
     <td>${item.NewConfirmed}</td>
-    <td style="color: red;">${item.NewDeaths}</td>
+    <td style="color: red;">-->${item.NewDeaths}<--</td>
     <td>${item.NewRecovered}</td>
     `)
 }
